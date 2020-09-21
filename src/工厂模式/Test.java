dbbdb035e78844aeff6c13b5b5bf388aa3fbe68e
chelly <591878545@qq.com>
2020-09-21 16:44:20 +0800

    更新1

--- src/工厂模式/Test.java
+++ src/工厂模式/Test.java
@@ -3,6 +3,9 @@ package 工厂模式;
 import java.util.Random;
 
 public class Test {
+    private int id;
+    private String title;
+
     public static void main(String[] args) {
         Random random = new Random();
         for (int i = 0; i <10 ; i++) {
